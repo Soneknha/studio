9b00e1221a73c3c28c701b7f8451269c740cdf9d
Paulo Junior <prakaji69@gmail.com>
2025-11-28 06:20:56 +0000

    esses são os meu dados do meu projeto
    // Import the functions you need

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-8787199924-6f7b0",
-  "appId": "1:8552448:web:76b53f76f7fb33f366d8e5",
-  "apiKey": "AIzaSyAgesY0yQCBnYz_1QLlrxGAPF_Ux02RAl0",
-  "authDomain": "studio-8787199924-6f7b0.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "8552448"
+  apiKey: "AIzaSyAgesY0yQCBnYz_1QLlrxGAPF_Ux02RAl0",
+  authDomain: "studio-8787199924-6f7b0.firebaseapp.com",
+  projectId: "studio-8787199924-6f7b0",
+  storageBucket: "studio-8787199924-6f7b0.firebasestorage.app",
+  messagingSenderId: "8552448",
+  appId: "1:8552448:web:76b53f76f7fb33f366d8e5"
 };
